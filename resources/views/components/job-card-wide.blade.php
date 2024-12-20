5fe4fd9710efdae7dd2c8005ed9d4848aed3806c
shonali16 <147215181+shonali16@users.noreply.github.com>
2024-12-19 22:53:16 -0500

    Initial commit

---
+++ resources/views/components/job-card-wide.blade.php
@@ -0,0 +1,29 @@
+<div class="p-4 bg-white/5 rounded-xl flex gap-x-6">
+    <div>
+        <x-employer-logo />
+    </div>
+
+
+
+    <div>
+
+    <a href="#" class="self-start text-sm text-gray-700">Laracasts</a>
+
+    </div>
+
+
+    <div class="py-8 font-bold flex-1">
+        <h3>Video Producer</h3>
+        <p>Full Time - From $60,000</p>
+    </div>
+
+
+        <div class="mt-6 space-y-6">
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+        </div>
+
+
+
+</div>
---
+++ resources/views/components/tag.blade.php
@@ -0,0 +1,1 @@
+<a href="#" class="bg-white/10 hover:bg-white/25 px-3 py-1 rounded-xl text-2xs  font-bold transition-colors duration-300">{{ $slot }}</a>
